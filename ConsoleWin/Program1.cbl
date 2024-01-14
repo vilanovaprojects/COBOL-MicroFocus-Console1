9b91bcec5d95cecc1c82ca29cb94e20e25669d2c
vilanovaprojects <mcvmoises@gmail.com>
2024-01-15 00:23:54 +0100

    menu02 y parte del 3

--- ConsoleWin/Program1.cbl
+++ ConsoleWin/Program1.cbl
@@ -50,18 +50,15 @@
       *============================================================
       * ************CURSOR****************************************
       *============================================================
-      *COMPATIBLE PARA DB2 VERSION 6
-      *           EN ESTA VERSION NO HAY SCROLL CURSOR
-
-           EXEC SQL
-               DECLARE CURS1 CURSOR WITH    RETURN FOR
-                   SELECT CIF
-                        , NOMBRE
-                        , DIRECCION
-                        , TLF
-                   FROM USRDATOS
-                   ORDER BY CIF
-           END-EXEC.
+      *    EXEC SQL
+      *        DECLARE CURS1 CURSOR FOR
+      *            SELECT CIF
+      *                 , NOMBRE
+      *                 , DIRECCION
+      *                 , TLF
+      *            FROM USRDATOS
+      *            ORDER BY CIF
+      *    END-EXEC.
 
       * CAMPOS PARA ERROR DE DB2
        01 FILLER PIC X(20) VALUE 'SQLER678901234567890'.
@@ -78,30 +75,48 @@
            03 WM00-OPC PIC X       value spaces.
 
        01 WM-01.
-         03 WM01-CIF PIC X(09)     value spaces.
-         03 WM01-NOM PIC X(20)     value spaces.
-         03 WM01-DIR PIC X(35)     value spaces.
-         03 WM01-TLF PIC X(09)     value spaces.
-         03 WM01-COR PIC X(20)     value spaces.
+         03 WM01-CIF   PIC X(09)   value spaces.
+         03 WM01-NOM   PIC X(20)   value spaces.
+         03 WM01-DIR   PIC X(35)   value spaces.
+         03 WM01-TLF   PIC X(09)   value spaces.
+         03 WM01-COR   PIC X(20)   value spaces.
 
        01 WM-02.
-         03 WM02-CIF PIC X(09) value spaces.
-         03 WM02-NOM PIC X(20) value spaces.
-         03 WM02-DIR PIC X(35) value spaces.
-         03 WM02-TLF PIC X(09) value spaces.
-         03 WM02-COR PIC X(20) value spaces.
+         03 WM02-CIF   PIC X(09)   value spaces.
+         03 WM02-NOM   PIC X(20)   value spaces.
+         03 WM02-DIR   PIC X(35)   value spaces.
+         03 WM02-TLF   PIC X(09)   value spaces.
+         03 WM02-COR   PIC X(20)   value spaces.
 
        01 WM-03.
-         03 WM03-FILA1 PIC X(76) value spaces.
-         03 WM03-FILA2 PIC X(76) value spaces.
-         03 WM03-FILA3 PIC X(76) value spaces.
-         03 WM03-FILA4 PIC X(76) value spaces.
-         03 WM03-FILA5 PIC X(76) value spaces.
-         03 WM03-FILA6 PIC X(76) value spaces.
-         03 WM03-FILA7 PIC X(76) value spaces.
-         03 WM03-FILA8 PIC X(76) value spaces.
-         03 WM03-FILA9 PIC X(76) value spaces.
-         03 WM03-FILA0 PIC X(76) value spaces.
+         03 WM03-FILA1 PIC X(76)   value spaces.
+         03 WM03-FILA2 PIC X(76)   value spaces.
+         03 WM03-FILA3 PIC X(76)   value spaces.
+         03 WM03-FILA4 PIC X(76)   value spaces.
+         03 WM03-FILA5 PIC X(76)   value spaces.
+         03 WM03-FILA6 PIC X(76)   value spaces.
+         03 WM03-FILA7 PIC X(76)   value spaces.
+         03 WM03-FILA8 PIC X(76)   value spaces.
+         03 WM03-FILA9 PIC X(76)   value spaces.
+         03 WM03-FILA0 PIC X(76)   value spaces.
+
+       01 TABLE-1.
+         02 DEPT OCCURS 10 TIMES.
+           05 EMPNO PIC X(6).
+           05 LASTNAME.
+             49 LASTNAME-LEN PIC S9(4) BINARY.
+             49 LASTNAME-TEXT PIC X(15).
+           05 WORKDEPT PIC X(3).
+           05 JOB PIC X(8).
+       01 TABLE-2.
+         02 IND-ARRAY OCCURS 10 TIMES.
+           05 INDS PIC S9(4) BINARY OCCURS 4 TIMES.
+
+       01 comodin pic x.
+       01 DATANUM PIC 999.
+       01 FINCURSOR PIC X(1).
+           88 FIN-CURSOR VALUE 'Y'.
+           88 NFIN-CURSOR VALUE 'N'.
 
        01 MSG-ERR PIC X(74) value spaces.
 
@@ -160,7 +175,7 @@
          05 WI02-CIF PIC X(9) LINE 10 COLUMN 26 USING WM02-CIF FOREGROUND-COLOR 3.
          05 LINE 12 COLUMN 10 VALUE IS '        NOMBRE:' FOREGROUND-COLOR 2.
          05 WI02-NOM PIC X(20) LINE 12 COLUMN 26 FROM WM02-NOM FOREGROUND-COLOR 14.
-         05 LINE 13 COLUMN 10 VALUE IS '     DIRECCIÓN:' FOREGROUND-COLOR 2.
+         05 LINE 13 COLUMN 10 VALUE IS '     DIRECCION:' FOREGROUND-COLOR 2.
          05 WI02-DIR PIC X(35) LINE 13 COLUMN 26 FROM WM02-DIR FOREGROUND-COLOR 14.
          05 LINE 14 COLUMN 10 VALUE IS '           TLF:' FOREGROUND-COLOR 2.
          05 WI02-TLF PIC X(9) LINE 14 COLUMN 26 FROM WM02-TLF FOREGROUND-COLOR 14.
@@ -199,11 +214,23 @@
          05 LINE 24 COLUMN 13 VALUE IS 'INTRO=CARGAR' FOREGROUND-COLOR 1.
          05 LINE 24 COLUMN 30 VALUE IS 'F10=RETROCEDER' FOREGROUND-COLOR 1.
          05 LINE 24 COLUMN 47 VALUE IS 'F11=SIGUIENTES' FOREGROUND-COLOR 1.
+         05 WI02-CIF PIC X(1) LINE 21 COLUMN 26 USING comodin FOREGROUND-COLOR 0.
+
 
 
        PROCEDURE DIVISION.
 
-           GO TO PRUEBAS.
+      *    GO TO PRUEBAS.
+
+           EXEC SQL
+               DECLARE CURS1 CURSOR FOR
+                   SELECT CIF
+                        , NOMBRE
+                        , DIRECCION
+                        , TLF
+                   FROM USRDATOS
+                   ORDER BY CIF
+           END-EXEC.
 
       *
       * EXCEPTIONES SQL DB2
@@ -264,11 +291,11 @@
 
        OTHER-PARRAFO.
        DISPLAY CLEANING.
-
-
            stop "PAUSA".
 
-
+      *============================================================
+      * -----------MENU01-----------------------------------------
+      *============================================================
        PARRAFO-MENU01.
            DISPLAY CLEANING.
            initialize WM-01.
@@ -312,6 +339,9 @@
                   , :WM01-TLF
                   , :WM01-COR)
            END-EXEC.
+           EXEC SQL COMMIT END-EXEC.
+
+
 
            IF SQLCODE = 0
                MOVE 'GUARDADO CORRECTAMENTE.' TO MSG-ERR
@@ -326,30 +356,116 @@
            perform PARRAFO-MENU01.
 
 
+      *============================================================
+      * -----------MENU02-----------------------------------------
+      *============================================================
        PARRAFO-MENU02.
            DISPLAY CLEANING.
+
+           call x"af" using flag
+                            user-key-control.
+
            display MENU02.
+
            accept MENU02.
+
+           INITIALIZE MSG-ERR.
+           evaluate key-type
+               when 0
+                   continue
+
+               when 1
+                   if key-code-1 = 3 THEN
+                       perform PARRAFO-MENU00
+                   ELSE
+                       perform PARRAFO-MENU02
+                   end-if
+           end-evaluate.
+
+           exec sql
+             connect 'sa' identified by 'Pas$123456' at 'cobolDB' using 'SQLADO32'
+           end-exec.
+
+           EXEC SQL
+                SELECT
+                     CIF
+                   , NOMBRE
+                   , DIRECCION
+                   , TLF
+                   , CORREO
+                INTO
+                     :WM02-CIF
+                   , :WM02-NOM
+                   , :WM02-DIR
+                   , :WM02-TLF
+                   , :WM02-COR
+                FROM USRDATOS
+                WHERE CIF = :WM02-CIF
+           END-EXEC.
+
+           EVALUATE TRUE
+               WHEN SQLCODE = 0
+                   MOVE 'REGISTRO ENCONTRADO' TO MSG-ERR
+
+               WHEN SQLCODE = +100
+                   MOVE 'LA EMPRESA NO EXISTE.' TO MSG-ERR
+                   initialize WM-02
+
+               WHEN OTHER
+      *            RECUPERAR POSIBLE ERROR DE DB2
+                   MOVE 'ERROR DESCONOCIDO. CONTACTE CON SOPORTE: correo@moisescampaña.es' TO MSG-ERR
+                   PERFORM G999-ERROR-DB2
+           END-EVALUATE.
+
+           perform PARRAFO-MENU02.
+
+      *============================================================
+      * -----------MENU03-----------------------------------------
+      *============================================================
        PARRAFO-MENU03.
-           display MENU03.
-           accept MENU02.
+           DISPLAY CLEANING.
 
-           
-
-
-           accept campo1.
-           accept campo1.
-
-
-
-
-
-
+           call x"af" using flag
+                            user-key-control.
 
            display MENU03.
-           accept campo1.
+
+           accept MENU03.
+
+           INITIALIZE MSG-ERR.
+           evaluate key-type
+               when 0
+                   continue
+
+               when 1
+                   evaluate key-code-1
+                       when 3
+                           perform PARRAFO-MENU00
+                       when 10
+                           IF NFIN-CURSOR
+                               ADD 10 TO DATANUM
+                           END-IF
+                       when 11
+                           IF DATANUM > 0
+                               SUBTRACT 10 FROM DATANUM GIVING DATANUM
+                           END-IF
+                       when other
+                           perform PARRAFO-MENU03
+                   end-evaluate
+           end-evaluate.
+
+
+           EXEC SQL OPEN CURS1 END-EXEC.
+
+           EXEC SQL
+             FETCH FROM CURS1 INTO :WM03-FILA1, :WM03-FILA2       
+                    , :WM03-FILA2, :WM03-FILA4
+           END-EXEC.
+
+
+           perform PARRAFO-MENU03.
+
        Leer-base-de-datos.
-
 
 
        enable-keys.
@@ -381,17 +497,29 @@
 
        PRUEBAS.
 
+           move "algo" to campo1.
+
            exec sql
              connect 'sa' identified by 'Pas$123456' at 'cobolDB' using 'SQLADO32'
            end-exec
-
-           move "algo" to campo1.
        
            EXEC SQL
+            
              INSERT
                INTO Table_1 (campo11, campo22)
                VALUES (:campo1, :campo1)
+
+
            END-EXEC.
+
+           EXEC SQL COMMIT END-EXEC.
+
+
+           EXEC SQL WHEN SQLERROR
+           DISPLAY "Error SQL: " SQLCODE
+           DISPLAY "Mensaje de Error: " SQLERRM
+           end-exec.
+           accept campo1
 
            EXEC SQL
              select
@@ -402,8 +530,9 @@
              from Table_1
            end-exec.
 
-           display "aquí debajo va el campo1:" with blank screen.
+           display "aquí debajo va el campo1:".
            display campo1.
+           display SQLERRM
            display campo2.
            accept campo1.
            display "*****************".
